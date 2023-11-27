6a139d6d4231ee5750742f23ddf785067356b08d
omshewale <omshewale030@gmail.com>
2023-11-27 11:31:13 -0700

    server running concurrently and fetching locations in Homescreen.jsx

--- src/Homescreen.jsx
+++ src/Homescreen.jsx
@@ -1,22 +1,54 @@
 import React, {useState, useEffect} from "react";
-import Databse from "./database";
+
 
 const Homescreen = () => {
-    // const query = 'SELECT * FROM weather_app.location';
-    // const {data,error} = Databse(query);
-    // if (error) {
-    //     return <div>error</div>
-    // }
+    const [locations, setLocations] = useState([]);
+    const [selectedLocation, setSelectedLocation] = useState(null);
+
+
+    useEffect(() => {
+        const fetchLocations = async () => {
+            try {
+                const response = await fetch("http://localhost:3001/api/v1/locations");
+
+                if(!response.ok) {
+                    throw new Error("HTTP Error! status: " + response.status);
+                }
+                const data = await response.json();
+                setLocations(data);
+            }catch (error) {
+                console.log(error);
+            }
+        };
+        fetchLocations();
+    }, []);
+    const handleLocationChange = (event) => {
+        const selectedLocationId = event.target.value;
+        const location = locations.find((loc) => loc.id === selectedLocationId);
+        setSelectedLocation(location);
+    };
+
     return (
         <div>
-            <h2>Weather App</h2>
-            <h3>Locations</h3>
-            <ul>
-                <li>Location 1</li>
-                {/*{data.map((location) => {*/}
-                {/*    return <li key={location.locationid}>{location.city}</li>*/}
-                {/*})}*/}
-            </ul>
+            <h2>Home Screen</h2>
+            <label>
+                Select Location:
+                <select onChange={handleLocationChange}>
+                    <option value="">Select a location</option>
+                    {locations.map((location) => (
+                        <option key={location.id} value={location.id}>
+                            {`${location.city}, ${location.country}`}
+                        </option>
+                    ))}
+                </select>
+            </label>
+
+            {selectedLocation && (
+                <div>
+                    <p>Selected Location: {`${selectedLocation.city}, ${selectedLocation.country}`}</p>
+                    {/* Render other components or details based on the selected location */}
+                </div>
+            )}
         </div>
     );
 };
